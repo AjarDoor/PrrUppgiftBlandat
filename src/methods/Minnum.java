b7a1435f0649763884a52e42eb1ce8756c743c28
AjarDoor <bulbofsaur@gmail.com>
2018-10-19 15:05:44 +0200

    New repo, Method package
    Created a new repository with a package to put my methods into. Created two methods that return the smallest and largest number from two inputted numbers, respectively.

---
+++ src/methods/Minnum.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class Minnum {
+	
+	
+	public static int min(int a, int b) {
+		if (a > b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
+	public static int max(int c, int d) {
+		if (c < d) {
+			return c;
+		}else {
+			return d;
+		}
+	}
+	
+
+
+
+}
