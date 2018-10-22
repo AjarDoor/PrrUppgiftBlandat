8e4c8ed9c445ec23bca59593198e6ab526d06840
AjarDoor <bulbofsaur@gmail.com>
2018-10-22 09:01:01 +0200

    Fixed alot of code and added method for reverse

---
+++ src/uppgift1prr/Methods.java
@@ -0,0 +1,47 @@
+package uppgift1prr;
+
+public class Methods {
+	
+	
+	public static int min(int a, int b) {
+		if (a < b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
+	public static int max(int c, int d) {
+		if (c > d) {
+			return c;
+		}else {
+			return d;
+		}
+	}
+	public static String reverse(String str) {
+		String reverse = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reverse = (reverse + str.charAt(i));
+		}
+		return reverse;
+	}
+	public static int tiotal(int i) {
+	i = ( i / 10);
+	if (i < 1) {
+		i = -1;
+	}
+	return i;
+	}
+	public static int backwards(int i) {
+		String yes = ;
+		if (i < 10){
+			return i;
+		}else {
+			for (b = yes.length(); ) {
+				
+			}
+		}
+			
+	}
+
+
+}
---
+++ src/uppgift1prr/Methodtesting.java
@@ -0,0 +1,17 @@
+package uppgift1prr;
+
+public class Methodtesting {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Methods.min(5, 6));
+		System.out.println("Min färdig");
+		
+		System.out.println(Methods.max(77, 12));
+		System.out.println("Max färdig");
+		
+		System.out.println(Methods.tiotal(2));
+		System.out.println("Tiotal färdig");
+	}
+
+}
