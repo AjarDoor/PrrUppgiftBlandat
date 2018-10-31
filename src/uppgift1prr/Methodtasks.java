add19bc761fc5fb40f95475a8396cee1b250ddef
AjarDoor <bulbofsaur@gmail.com>
2018-10-31 16:49:30 +0100

    Task start
    Created a class to start using my methods for the given tasks at the bottom of the assignment

---
+++ src/uppgift1prr/Methodtasks.java
@@ -0,0 +1,16 @@
+package uppgift1prr;
+
+public class Methodtasks {
+	
+	public static void main(String[] args) {
+		System.out.println(Methods.min(25, 33));
+		System.out.println("Uppgift 1 färdig");
+		
+		System.out.println(Methods.isPrim(29));
+		System.out.println("Uppgift 2 färdig");
+		
+		System.out.println(Methods.askForName());
+		System.out.println("Uppgift 3 färdig");
+	}
+
+}
