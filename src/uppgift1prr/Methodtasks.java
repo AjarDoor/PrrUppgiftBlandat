676bcc3285ab4839c7fc1d6d23ea4eb4420068ba
AjarDoor <bulbofsaur@gmail.com>
2018-11-03 14:26:40 +0100

    Task 6 done

--- src/uppgift1prr/Methodtasks.java
+++ src/uppgift1prr/Methodtasks.java
@@ -18,7 +18,8 @@ public class Methodtasks {
 		System.out.println(Methods.fakultet(8));
 		System.out.println("Uppgift 5 färdig");
 		
-		
+		System.out.println(Methods.tiotal(83));
+		System.out.println("Uppgift 6 färdig");
 	}
 
 }
